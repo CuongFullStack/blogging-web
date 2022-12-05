53b7ebd8a759a633265b5c6d2121cbfc5a5912b8
Vu Cuong <vuvietcuong1351998@gmail.com>
2022-12-05 16:11:28 +0700

    update 4: complete the sign up form

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Label } from "components/label";
 import { Input } from "components/input";
 import { Field } from "components/field";
 import { useForm } from "react-hook-form";
-import { IconEyeClose } from "components/icon";
+import { IconEyeClose, IconEyeOpen } from "components/icon";
+import { Button } from "components/button";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db } from "firebase-app/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
 
 const SignUpPageStyles = styled.div`
   /* background-color: ${(props) => props.theme.primary}; */
@@ -27,16 +35,65 @@ const SignUpPageStyles = styled.div`
   }
 `;
 
+const schema = yup.object({
+  fullname: yup.string().required("Please enter your fullname"),
+  email: yup
+    .string()
+    .email("Please enter valid email address")
+    .required("Please enter your email"),
+  password: yup
+    .string()
+    .min(8, "Your password must be at least 8 characters or greater")
+    .required("Please enter your password"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+});
+
 const SignUpPage = () => {
+  const navigate = useNavigate();
   const {
     control,
     handleSubmit,
     formState: { errors, isValid, isSubmitting },
     watch,
-  } = useForm({});
-  const handleSignUp = (values) => {
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+  const handleSignUp = async (values) => {
+    if (!isValid) return;
     console.log(values);
+    const user = await createUserWithEmailAndPassword(
+      auth,
+      values.email,
+      values.password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: values.fullname,
+    });
+    const colRef = collection(db, "users");
+    await addDoc(colRef, {
+      fullname: values.fullname,
+      email: values.email,
+      password: values.password,
+    });
+    toast.success("Sign up successfully!!!");
+    navigate("/");
   };
+  const [togglePassword, setTogglePassword] = useState(false);
+  console.log(errors);
+  useEffect(() => {
+    const arrErroes = Object.values(errors);
+    if (arrErroes.length > 0) {
+      toast.error(arrErroes[0]?.message, {
+        pauseOnHover: false,
+        delay: 0,
+      });
+    }
+  }, [errors]);
+
   return (
     <SignUpPageStyles>
       <div className="container">
@@ -70,14 +127,54 @@ const SignUpPage = () => {
           <Field className="field">
             <Label htmlFor="password">Password</Label>
             <Input
-              type="password"
+              type={togglePassword ? "text" : "password"}
               name="password"
               placeholder="Enter your password"
               control={control}
             >
-              <IconEyeClose className="input-icon"></IconEyeClose>
+              {!togglePassword ? (
+                <IconEyeClose
+                  onClick={() => setTogglePassword(true)}
+                ></IconEyeClose>
+              ) : (
+                <IconEyeOpen
+                  onClick={() => setTogglePassword(false)}
+                ></IconEyeOpen>
+              )}
             </Input>
           </Field>
+
+          <Field className="field">
+            <Label htmlFor="passwordConfirmation">Confirm your password</Label>
+            <Input
+              type={togglePassword ? "text" : "password"}
+              name="passwordConfirmation"
+              placeholder="Re-enter your password"
+              control={control}
+            >
+              {!togglePassword ? (
+                <IconEyeClose
+                  onClick={() => setTogglePassword(true)}
+                ></IconEyeClose>
+              ) : (
+                <IconEyeOpen
+                  onClick={() => setTogglePassword(false)}
+                ></IconEyeOpen>
+              )}
+            </Input>
+          </Field>
+
+          <Button
+            type="submit"
+            style={{
+              maxWidth: "300px",
+              margin: "0 auto",
+            }}
+            disabled={isSubmitting}
+            isLoading={isSubmitting}
+          >
+            Sign up
+          </Button>
         </form>
       </div>
     </SignUpPageStyles>
